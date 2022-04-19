148658f663c0a38ec18c115d236fe22a790ceed2
Erick <ericksilvaredes@gmail.com>
2022-04-18 22:43:02 -0300

    primeira versão

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let toggle = document.querySelector('#toggle_menu');
+toggle.addEventListener('click', () => {
+    let menu = document.querySelector('.container_menu')
+        menu.classList.toggle("active")
+} )
+
+
+
